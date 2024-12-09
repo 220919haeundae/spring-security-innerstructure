695cfb5c4283eff4ae9ab2d013f7131cb653e8f4
220919haeundae <113912025+220919haeundae@users.noreply.github.com>
2024-12-09 21:10:13 +0900

    feat: spring security 내부 구조 학습(11)  - AnonymousAuthenticationFilter 동작 학습  - ExceptionTranslationFilter 동작 학습  - AuthorizationFilter 동작 학습

--- src/main/java/com/example/testsecurityinnerfunction/controller/MainController.java
+++ src/main/java/com/example/testsecurityinnerfunction/controller/MainController.java
@@ -1,29 +1,25 @@
 package com.example.testsecurityinnerfunction.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 @Controller
 public class MainController {
 
-    @GetMapping("/testfilterbefore1")
-    public String before1() {
-
-        return "forward:/testfilterafter";
-    }
-
-    @GetMapping("/testfilterbefore2")
-    public String before2() {
-
-        return "redirect:/testfilterafter";
-    }
-
-    @GetMapping("/testfilterafter")
+    @GetMapping("/")
     @ResponseBody
-    public String after() {
+    public String mainPage(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        return "hihihi";
+        request.logout();
+        request.login("username", "password");
+
+        return "aaa";
     }
+
 }
